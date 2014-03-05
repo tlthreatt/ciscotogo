a062c87de0990b2d06765aa53e6ca6748ac72ee2
mmoisen <mmoisen@cisco.com>
2014-03-05 10:52:35 -0800

    jj

--- CiscoToGoHackIT/src/main/java/com/cisco/ciscotogo/dao/CategoryDao.java
+++ CiscoToGoHackIT/src/main/java/com/cisco/ciscotogo/dao/CategoryDao.java
@@ -2,15 +2,26 @@ package com.cisco.ciscotogo.dao;
 
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 import com.cisco.ciscotogo.model.Category;
 import com.cisco.ciscotogo.model.Location;
 
 
 public class CategoryDao {
+	Configuration configuration = new Configuration().configure();
+	ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();
+	SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	
 
+	
+	
+	
+	//session.save(location);
+	
 
 }
